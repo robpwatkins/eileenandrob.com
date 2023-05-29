554c1d0347e14165ebb4a9edb678f3853c58e1bd
robpwatkins <robpwatkins@gmail.com>
2023-05-29 17:19:09 -0500

    manually adding photos to gallery and carousel, removing js functions

--- index.js
+++ index.js
@@ -18,107 +18,73 @@ window.addEventListener('DOMContentLoaded', () => {
     : Array.from(navItems).find(item => pathname.includes(item.innerText.toLowerCase()));
 
   currentNav.classList.add('active');
-})
 
-document.querySelector('.toggle-nav').addEventListener('click', () => {
-  const ul = document.querySelector('ul');
+  document.querySelector('.toggle-nav').addEventListener('click', () => {
+    const ul = document.querySelector('ul');
 
-  if (navOpen) {
-    closeNav.style.display = 'none';
-    openNav.style.display = 'block';
-    ul.style.display = 'none';
-    nav.style.backgroundColor = 'var(--darkgreen)';
-    body.style.overflow = 'scroll';
-    navOpen = false;
-  } else {
-    openNav.style.display = 'none';
-    closeNav.style.display = 'block';
-    ul.style.display = 'flex';
-    nav.style.backgroundColor = 'unset';
-    body.style.overflow = 'hidden';
-    navOpen = true;
-  }
-});
-
-registryItem.addEventListener('mouseenter', (e) => {
-  squareArrow.style.opacity = 1;
-});
-
-registryItem.addEventListener('mouseleave', (e) => {
-  squareArrow.style.opacity = 0;
-});
-
-let activeImgContainer;
-
-const renderGallery = async () => {
-  const handleThumbnailClick = (e) => {
-    document.querySelector('body').style.overflow = 'hidden';
-
-    carouselModal.style.top = `${window.pageYOffset}px`;
-    carouselModal.classList.add('active');
-
-    const [currentImg] = e.target.classList;
-    const carouselImg = document.querySelector(`.carousel .modal .${currentImg}`);
-    activeImgContainer = carouselImg.parentElement;
-    activeImgContainer.style.display = 'flex';
-  }
-
-  let count = 1;
-  
-  while (count < 29) {
-    const img = document.createElement('img');
-    img.className = `img-${count}`;
-    img.src = `./img/gallery/${count}.webp`;
-
-    const imgContainer = document.createElement('div');
-    imgContainer.className = 'img-container';
-    imgContainer.onclick = handleThumbnailClick;
-
-    img.onload = () => imgContainer.appendChild(img);
-    thumbnails.appendChild(imgContainer);
-
-    const modalImgContainer = document.createElement('div');
-    modalImgContainer.classList.add('modal-img-container');
-    const modalImg = img.cloneNode();
-
-    modalImgContainer.insertAdjacentHTML('beforeend', `
-      <div class="controls">
-        <div class="move move-left"><</div>
-        <div class="move move-right">></div>
-      </div>
-    `);
-
-    modalImg.onload = () => modalImgContainer.insertAdjacentElement('afterbegin', modalImg);
-    carouselModal.appendChild(modalImgContainer);
-
-    count++;
-  }
-};
-
-if (gallery) {
-  renderGallery();
-  
-  carouselModal.addEventListener('click', (e) => {
-    if (
-      carouselModal.classList.contains('active')
-      && !e.target.classList.contains('controls')
-      && !e.target.classList.contains('move')
-    ) {
-      activeImgContainer.style.display = 'none';
-      carouselModal.classList.remove('active');
-      document.querySelector('body').style.overflow = 'scroll';
+    if (navOpen) {
+      closeNav.style.display = 'none';
+      openNav.style.display = 'block';
+      ul.style.display = 'none';
+      nav.style.backgroundColor = 'var(--darkgreen)';
+      body.style.overflow = 'scroll';
+      navOpen = false;
+    } else {
+      openNav.style.display = 'none';
+      closeNav.style.display = 'block';
+      ul.style.display = 'flex';
+      nav.style.backgroundColor = 'unset';
+      body.style.overflow = 'hidden';
+      navOpen = true;
     }
   });
 
-  document.querySelectorAll('.move').forEach(el => el.addEventListener('click', (e) => {
-    let sibling;
-    
-    if (e.target.classList.contains('move-left')) {
-      sibling = activeImgContainer.previousElementSibling;
-    } else sibling = activeImgContainer.nextElementSibling;
+  registryItem.addEventListener('mouseenter', (e) => {
+    squareArrow.style.opacity = 1;
+  });
 
-    activeImgContainer.style.display = 'none';
-    sibling.style.display = 'flex';
-    activeImgContainer = sibling;
-  }));
-}
+  registryItem.addEventListener('mouseleave', (e) => {
+    squareArrow.style.opacity = 0;
+  });
+
+  let activeImgContainer;
+
+
+  if (gallery) {
+    thumbnails.addEventListener('click', (e) => {
+      document.querySelector('body').style.overflow = 'hidden';
+    
+      carouselModal.style.top = `${window.pageYOffset}px`;
+      carouselModal.classList.add('active');
+    
+      const [currentImg] = e.target.classList;
+      const carouselImg = document.querySelector(`.carousel .modal .${currentImg}`);
+      activeImgContainer = carouselImg.parentElement;
+      activeImgContainer.style.display = 'flex';
+    });
+    
+    carouselModal.addEventListener('click', (e) => {
+      if (
+        carouselModal.classList.contains('active')
+        && !e.target.classList.contains('controls')
+        && !e.target.classList.contains('move')
+      ) {
+        activeImgContainer.style.display = 'none';
+        carouselModal.classList.remove('active');
+        document.querySelector('body').style.overflow = 'scroll';
+      }
+    });
+
+    document.querySelectorAll('.move').forEach(el => el.addEventListener('click', (e) => {
+      let sibling;
+      
+      if (e.target.classList.contains('move-left')) {
+        sibling = activeImgContainer.previousElementSibling;
+      } else sibling = activeImgContainer.nextElementSibling;
+
+      activeImgContainer.style.display = 'none';
+      sibling.style.display = 'flex';
+      activeImgContainer = sibling;
+    }));
+  }
+});
